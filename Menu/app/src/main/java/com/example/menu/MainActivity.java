63634451f261d019f870ecd7e15d4ab5c2a75ad0
zied <zied.hack@gmail.com>
2019-11-19 21:31:32 +0100

    Menu

---
+++ Menu/app/src/main/java/com/example/menu/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.menu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView t1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        t1 = findViewById(R.id.textView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+
+        if(id == R.id.red){
+        t1.setTextColor(Color.RED);
+
+
+        }
+        if(id == R.id.green){
+            t1.setTextColor(Color.GREEN);
+
+        }
+        if(id == R.id.finish){
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
---
+++ Pizzacommande/app/src/main/java/com/example/pizza_commande/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.pizza_commande;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
